f273045bf45d6eb3e67d8fcd93b7439338fed6ae
Kenny Tang <kenny.tang@icloud.com>
2015-05-25 03:30:18 +0800

    Refactor & documentation

--- README.md
+++ README.md
@@ -1,6 +1,100 @@
 # Express IP Access Control
 An express.js middleware for access control.
 
-Not tested yet.
+---
+## Installation
 
-Will test and document soon.
+`npm install ipaddr.js`
+
+---
+## Feature
+
+* Support **Express**.
+* Control who can access resources base on IP address.
+* Support **IPv4, IPv6, CIDR format & IPv4 mapped IPv6 addresses** (thanks to [ipaddr.js](https://github.com/whitequark/ipaddr.js)).
+* Deny mode (**Blacklist**) & Allow mode (**Whitelist**), similar [Apache Access Control](https://httpd.apache.org/docs/2.2/howto/access.html).
+* Choose from **connection address or real address**. You may find it useful if you are behind proxy and needed to reject direct access.
+* Custom action on denied. (**Redirect or show error message**)
+* Custom log function.
+
+---
+## Usage
+
+> If you are not familiar with Express and Express's middleware,
+> you are recommended to see these first.
+> * [Express](http://expressjs.com)
+> * [Express's middleware](http://expressjs.com/guide/using-middleware.html)
+
+```javascript
+var accessControl = require('express-ip-access-control');
+```
+
+Create middleware by calling `accessControl(options)` or directly load it into your app by `app.use(accessControl(options))`.
+
+---
+## Options
+
+```javascript
+var options = {
+	mode: 'deny',
+	denys: [],
+	allows: [],
+	forceConnectionAddress: false,
+	log: function(clientIp, access) {
+		console.log(clientIp + (access ? ' accessed.' : ' denied.'));
+	},
+
+	statusCode: 401,
+	redirectTo: '',
+	message: 'Unauthorized'
+};
+```
+
+### mode (default: `'deny'`)
+
+#### `'deny'` mode (Blacklist)
+
+Allow by default, only deny IP in the blacklist (`denys`) and not excluded by the whitelist (`allows`).
+
+#### `'allow'` mode (Whilelist)
+
+Deny by default, only allow IP in the whitelist (`allows`) and not excluded by the blacklist (`denys`).
+
+### denys (default: [])
+
+The blacklist. Works differently in different mode.
+
+### allows (default: [])
+
+The whitelist. Works differently in different mode.
+
+### forceConnectionAddress (default: false)
+
+If set to `true`, the connection address (`req.connection.remoteAddress`) will be used even `express.set('trust proxy', [])` is set. So that you can reject direct access if you are behind proxy and needed to do so.
+
+### log (default: Simple log function)
+
+Pass a log function or `false` to disable log.
+The function should have signature like this `Function(String clientIp, Boolean access)`.
+
+### statusCode (default: 401)
+
+The HTTP status code sent when denied. Set to 301 or 302 means redirect to `redirectTo`.
+
+### redirectTo (default: '')
+
+The URL to redirect when denied and `statusCode` is set to redirect.
+
+### message (default: 'Unauthorized')
+
+The message sent when denied and `statusCode` is not set to redirect.
+
+---
+## Repository
+
+You may find the source code at [GitHub](https://github.com/KennyTangHK/express-ip-access-control). Please feel free to report bugs and contribute your changes.
+
+---
+## License
+
+[MIT](LICENSE)
--- index.js
+++ index.js
@@ -1,75 +1,90 @@
 var Address = require('ipaddr.js');
 
-var isNumber = function(n) {
-	return !isNaN(parseFloat(n)) && isFinite(n);
-};
+var isNumeric = function(n) { return !isNaN(parseFloat(n)) && isFinite(n); };
 
 var ipMatch = function(clientIp, list) {
-	if (!clientIp || !Address.isValid(clientIp)) return false;
-	clientIp = Address.process(clientIp);
+	if (clientIp && Address.isValid(clientIp)) {
+		// `Address.process` return the IP instance in IPv4 or IPv6 form.
+		// It will return IPv4 instance if it's a IPv4 mapped IPv6 address
+		clientIp = Address.process(clientIp);
+		return list.some(function(e) {
+			// IPv6 address has 128 bits and IPv4 has 32 bits.
+			// Setting the fix bits to all bits in a CIDR address means only the specified address is allowed. No range.
+			e = e || '';
+			e = e.indexOf('/') === -1 ? e + '/128' : e;
 
-	return list.some(function(e) {
-		e = e || '';
-		e = e.indexOf('/') === -1 ? e + '/128' : e;
+			var range = e.split('/');
+			if (range.length === 2 && Address.isValid(range[0]) && isNumeric(range[1])) {
+				var ip = Address.parse(range[0]);
+				var bit = parseInt(range[1], 10);
 
-		var range = e.split('/');
-		if (range.length === 2 && Address.isValid(range[0]) && isNumber(range[1])) {
-			var ip = Address.parse(range[0]);
-			var byte = parseInt(range[1], 10);
-
-			if (clientIp.kind() === ip.kind()) {
-				return clientIp.match(ip, byte);
+				// `IP.kind()` return `'ipv4'` or `'ipv6'`. Only same type can be `match`.
+				if (clientIp.kind() === ip.kind()) {
+					return clientIp.match(ip, bit);
+				}
 			}
-		}
 
-		return false;
-	});
+			return false;
+		});
+	}
+
+	return false;
 };
 
 function AccessControl(opts) {
 	var _options = {
+		// mode: `'deny'` or `'allow'`. See README.md or below.
 		mode: 'deny',
+		// denys: List of denied IP (Specified IP / CIDR format).
 		denys: [],
+		// allows: List of allowed IP (Specified IP / CIDR format).
 		allows: [],
+		// forceConnectionAddress: Use the connection address (`req.connection.remoteAddress`) even `express.set('trust proxy', [])` is set.
+		forceConnectionAddress: false,
+		// log: Pass a log function or `false` to disable log.
+		// `Function(String clientIp, Boolean access)`
 		log: function(clientIp, access) {
 			console.log(clientIp + (access ? ' accessed.' : ' denied.'));
 		},
-		forceConnectionAddress: false,
+
+		// statusCode: The HTTP status code sent when denied. Set to 301 or 302 means redirect to `redirectTo`.
 		statusCode: 401,
-		message: 'Unauthorized',
-		redirectTo: ''
+		// redirectTo: The URL to redirect when denied and `statusCode` is set to redirect.
+		redirectTo: '',
+		// message: The message sent when denied and `statusCode` is not set to redirect.
+		message: 'Unauthorized'
 	};
 
 	opts = opts || {};
 	for (var p in opts) if (opts.hasOwnProperty(p)) _options[p] = opts[p];
-	_options.mode = _options.mode === 'allow';
+	_options.allowMode = _options.mode === 'allow';
 
 	return function(req, res, next) {
 		var clientIp = _options.forceConnectionAddress === true
 			? req.connection.remoteAddress
 			: req.ip || req.connection.remoteAddress;
 
-		var allow = function() {
-			if (typeof _options.log === 'function') _options.log.apply(null, [req.ip || req.connection.remoteAddress, true]);
-			next();
-		};
+		var inAllows = ipMatch(clientIp, _options.allows);
+		var inDenys = ipMatch(clientIp, _options.denys);
 
-		var deny = function() {
-			if (typeof _options.log === 'function') _options.log.apply(null, [req.ip || req.connection.remoteAddress, false]);
-			if (_options.statusCode === 301 || _options.statusCode === 302) {
-				res.redirect(_options.statusCode, _options.redirectTo);
-			} else {
-				res.status(_options.statusCode).end(_options.message);
-			}
-		};
+		// If it is `'allow'` mode, and the IP *IS NOT* in the whitelist (`allows`) list (or excluded by blacklist (`denys`)), deny it.
+		// If it is `'deny'` mode, and the IP *IS* in the blacklist (`denys`) list (and not excluded by whitelist (`allows`)), deny it.
+		var isDenied = (_options.allowMode === true && (inAllows && !inDenys) === false) || (_options.allowMode === false && (inDenys && !inAllows) === true);
 
-		var inAllows = ipMatch(clientIp, _options.allows), inDenys = ipMatch(clientIp, _options.denys);
-		if ((_options.mode === true && (inAllows && !inDenys) === false) || (_options.mode === false && (inDenys && !inAllows) === true)) {
-			deny();
+		if (typeof _options.log === 'function') {
+			_options.log.apply(null, [req.ip || req.connection.remoteAddress, !isDenied]);
+		}
+
+		if (isDenied) {
+			(_options.statusCode === 301 || _options.statusCode === 302)
+				? res.redirect(_options.statusCode, _options.redirectTo)
+				: res.status(_options.statusCode).end(_options.message);
 		} else {
-			allow();
+			next();
 		}
 	}
 }
+
+AccessControl.ipMatch = ipMatch;
 
 module.exports = exports = AccessControl;
